cec27e28279a4f250e44977d78c3d38198f1227f
Nathan Harris <derjyn@gmail.com>
2014-09-23 18:41:32 -0700

    Major Update
    * Complete theme overhaul
    * Updated to latest Flesh and Bones
    * Modified layout

--- soma.js
+++ soma.js
@@ -7,19 +7,19 @@ $(document).ready(function () {
 	// ParticleGround stuff
   $('#bg-fx').particleground({
   	minSpeedX: 0.2,
-  	maxSpeedX: 0.7,
+  	maxSpeedX: 0.5,
   	minSpeedY: 0.2,
-  	maxSpeedY: 0.7,
+  	maxSpeedY: 0.5,
   	directionX: 'center',		// center/up/down. center has the dots bouncing off edge of canvas
   	dirextionY: 'center',		// center/up/down. center has the dots bouncing off edge of canvas
   	density: 6000,					// particle density, one particle every n pixels (higher = less)
-  	dotColor: 'rgba(101,111,128,0.8)',
-  	lineColor: 'rgba(101,111,128,0.8)',
-  	particleRadius: 2,			// dot size
+  	dotColor: 'rgb(80,80,80)',
+  	lineColor: 'rgb(180,180,180)',
+  	particleRadius: 1,			// dot size
   	lineWidth: 0.3,
   	curvedLines: false,
   	proximity: 70,					// proximity before dots join
-  	parallax: true,
+  	parallax: false,
   	parallaxMultiplier: 25	// lower values make the parallax effect more extreme
 	});
 });
