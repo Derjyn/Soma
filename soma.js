98037ccd8334e6e56aa40584700fdd6804a944c3
Nathan Harris <derjyn@gmail.com>
2014-10-09 10:04:02 -0700

    Major Update
    * Added video header background
    * Cleaned up code a bit
    * Got down and dirty with Bones and Flesh relations

--- soma.js
+++ soma.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
   	directionX: 'center',		// center/up/down. center has the dots bouncing off edge of canvas
   	dirextionY: 'center',		// center/up/down. center has the dots bouncing off edge of canvas
   	density: 6000,					// particle density, one particle every n pixels (higher = less)
-  	dotColor: 'rgb(80,80,80)',
-  	lineColor: 'rgb(180,180,180)',
+  	dotColor: 'rgb(60,60,60)',
+  	lineColor: 'rgb(120,120,120)',
   	particleRadius: 1,			// dot size
   	lineWidth: 0.3,
   	curvedLines: false,
@@ -22,4 +22,15 @@ $(document).ready(function () {
   	parallax: false,
   	parallaxMultiplier: 25	// lower values make the parallax effect more extreme
 	});
+
+  $('#vid_header').videoBG({
+    mp4: 'assets/vid_header.mp4',
+    ogv: 'assets/vid_header.ogv',
+    webm: 'assets/vid_header.webm',
+    poster: 'assets/vid_header.png',
+    position: 'relative',
+    loop: true,
+    scale: false,
+    zIndex: -1
+  });
 });
